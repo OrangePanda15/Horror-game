748dfad923262b8b4755f42ccc515262b20e75d3
OrangePanda15 <Sparkles010203@gmail.com>
2023-10-12 04:30:41 +1100

    Player movement + Extras
    Set up the player movement and started working on getting interactables working

---
+++ Horror prototype/Assets/Scripts/Interactable/Interactable_Note.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_Note : MonoBehaviour, IInteractable
+{
+    // Exposed variables
+
+    // Component references
+
+    // Internal variables
+
+    public void Interact()
+    {
+
+    }
+}
