f5ad6a0157bd631177a62457b1568a8d1fc96f56
OrangePanda15 <Sparkles010203@gmail.com>
2023-10-18 20:25:01 +1100

    OrangeLib Updates + Transparent BG
    added comments to the functions in OrangeLib
    added a transparent background effect that allows the player to see their desktop through the windows in the house (effect does not work in editor)

--- Horror prototype/Assets/Scripts/Managers/Game_Manager.cs
+++ Horror prototype/Assets/Scripts/Managers/Game_Manager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.InteropServices;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Horror prototype/Assets/Scripts/Orange_Lib.cs
+++ Horror prototype/Assets/Scripts/Orange_Lib.cs
@@ -7,11 +7,15 @@ public class Orange_Lib
 {
     //  Orange Library
     //
-    //  This library is a set of functions which might make game development easier for
+    //  This library is a set of functions which might make game development easier for Orange Panda
 
+    /// <summary>
+    /// Calculates the angle between a vector2 and the x-axis in degrees anti-clockwise from the x-axis
+    /// </summary>
+    /// <param name="vec"></param>
+    /// <returns></returns>
     public static float Vector2ToAngle(Vector2 vec)
     {
-        //calculates the angle between a vector2 and the x-axis in degrees
         float angle =
             (
                 Mathf.Atan(vec.y /vec.x) /
@@ -22,25 +26,48 @@ public class Orange_Lib
         return angle;
     }
 
+    /// <summary>
+    /// Calculates a unit-vector angle degrees anti-clockwise from the x-axis
+    /// </summary>
+    /// <param name="angle"></param>
+    /// <returns></returns>
     public static Vector2 AngleToVector2(float angle)
     {
-        //calculates a unit-vector for which the angle in degrees between the x-axis and the vector is the input angle
         angle = angle * Mathf.Deg2Rad;
         Vector2 vec = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
 
         return vec;
     }
 
+    /// <summary>
+    /// Calculates the distance between two points assuming one can only move along either the x or y axis at a time
+    /// </summary>
+    /// <param name="pos1"></param>
+    /// <param name="pos2"></param>
+    /// <returns></returns>
     public static float ManhattanDistance(Vector2 pos1, Vector2 pos2)
     {
         return Mathf.Abs(pos1.x - pos2.x) + Mathf.Abs(pos1.y - pos2.y);
     }
 
+    /// <summary>
+    /// Returns a random item from an array
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="arr"></param>
+    /// <returns></returns>
     public static T GetRandomItemInArray<T>(T[] arr)
     {
         return arr[Mathf.FloorToInt(UnityEngine.Random.Range(0.0f, arr.Length))];
     }
 
+    /// <summary>
+    /// <br>returns a string containing items from an array</br>
+    /// <br>uses array type's default string conversion</br>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="arr"></param>
+    /// <returns></returns>
     public static string ArrayToString<T>(T[] arr)
     {
         string output = "";
