3f846f63f8d313e31bf2d9c37eae998709702329
OrangePanda15 <Sparkles010203@gmail.com>
2023-10-24 08:57:18 +1100

    Note text scale sync
    Made it so the relative size of the 3d and ui text of the note prefab are now synced such that they take up the same relative space on the background texture/model
    
    Made note model scale based on the texture used and an arbitrary scale value

--- Horror prototype/Assets/Scripts/Interactable/Interactable_Note.cs
+++ Horror prototype/Assets/Scripts/Interactable/Interactable_Note.cs
@@ -7,28 +7,36 @@ public class Interactable_Note : MonoBehaviour, IInteractable
 {
     [Header("Variables")]
     [TextArea] public string noteText;
-    public float textUIpadding;
+    [Range(0.0f, 0.5f)] public float textPadding;
+    public float characterSize;
+    public float modelScale;
     [Header("Asset References")]
+    public Font font;
     public AudioClip[] audioClipsActivate;
     public AudioClip[] audioClipsDeactivate;
 
     [Header("Component References")]
     public TextMesh text3d;
+    public GameObject model;
     public GameObject canvas;
+    public RectTransform textUIContainer;
     public Text textUI;
     public Image backgroundUI;
     public AudioSource audioSource;
     public Outline outline { get; set; }
 
+    // Internal Variables
+    char[] validLinebreakCharacters = new char[]
+    {
+        ' ',
+        '\n'
+    };
+
     void Start()
     {
-        text3d.text = noteText;
-        textUI.text = noteText;
+        InitializeNote();
         canvas.SetActive(false);
-
         outline = GetComponent<Outline>();
-
-        SetTextUIAnchors();
     }
 
     public void Interact()
@@ -49,6 +57,18 @@ public class Interactable_Note : MonoBehaviour, IInteractable
         audioSource.Play();
     }
 
+    public void InitializeNote()
+    {
+        text3d.text = noteText;
+        textUI.text = noteText;
+        text3d.font = font;
+        textUI.font = font;
+        SetTextUIAnchors();
+        ScaleModel();
+        NormalizeTextSizes();
+        HandleTextWrapping();
+    }
+
     public void SetTextUIAnchors()
     {
         float backgroundAspect = backgroundUI.rectTransform.rect.width / backgroundUI.rectTransform.rect.height;
@@ -57,16 +77,72 @@ public class Interactable_Note : MonoBehaviour, IInteractable
 
         float centerOffset = 0.5f * ratio;
 
-        textUI.rectTransform.anchorMin = new Vector2(0.5f - centerOffset, 0.0f);
-        textUI.rectTransform.anchorMax = new Vector2(0.5f + centerOffset, 1.0f);
+        textUIContainer.anchorMin = new Vector2(0.5f - centerOffset, 0.0f);
+        textUIContainer.anchorMax = new Vector2(0.5f + centerOffset, 1.0f);
+        textUIContainer.offsetMin = Vector2.zero;
+        textUIContainer.offsetMax = Vector2.zero;
 
-        textUI.rectTransform.offsetMin = new Vector2(textUIpadding, -textUIpadding);
-        textUI.rectTransform.offsetMax = new Vector2(-textUIpadding, -textUIpadding);
+        textUI.rectTransform.anchorMin = new Vector2(textPadding, textPadding);
+        textUI.rectTransform.anchorMax = Vector2.one - new Vector2(textPadding, textPadding);
+        textUIContainer.offsetMin = Vector2.zero;
+        textUIContainer.offsetMax = Vector2.zero;
+    }   
+    
+    public void ScaleModel()
+    {
+        Vector3 dimensions = modelScale * new Vector2(backgroundUI.sprite.rect.width / backgroundUI.sprite.rect.height, 1.0f);
+        model.transform.localScale = new Vector3(dimensions.x, 1.0f, dimensions.y);
+        text3d.transform.localPosition = 5.0f * new Vector3(dimensions.x, dimensions.y, 0.0f);
+        text3d.transform.localPosition *= 1.0f - (2.0f * textPadding);
+    }
 
-        Logger.Log(this, "Text UI anchors: " +
-            textUI.rectTransform.anchorMin.x.ToString() + ", " +
-            textUI.rectTransform.anchorMin.y.ToString() + ", " +
-            textUI.rectTransform.anchorMax.x.ToString() + ", " +
-            textUI.rectTransform.anchorMax.y.ToString());
-    }    
+    public void NormalizeTextSizes()
+    {
+        text3d.characterSize = characterSize * 4.0f * model.transform.localScale.z;
+        textUI.fontSize = Mathf.RoundToInt(characterSize * backgroundUI.rectTransform.rect.height);
+    }
+
+    public void HandleTextWrapping()
+    {
+        float fontSizeRatio = textUI.fontSize / font.fontSize;
+        string[] words = noteText.Split(validLinebreakCharacters, System.StringSplitOptions.RemoveEmptyEntries);
+
+        string output = "";
+        int maxLineCount = Mathf.FloorToInt((1.0f - (2.0f * textPadding)) / characterSize);
+        int currentLineCount = 0;
+        string currentLine = "";
+        float maxLineWidth = textUI.rectTransform.rect.width;
+        float currentLineWidth = 0.0f;
+        for (int i = 0; i < words.Length && currentLineCount < maxLineCount; i++)
+        {
+            float wordWidth = 0.0f;
+            CharacterInfo charInfo;
+            foreach (char letter in words[i])
+            {
+                font.GetCharacterInfo(letter, out charInfo);
+                float letterWidth = charInfo.advance * fontSizeRatio;
+                wordWidth += letterWidth;
+            }
+            font.GetCharacterInfo(' ', out charInfo);
+            wordWidth += charInfo.advance;
+            Logger.Log(this, wordWidth.ToString() + " " + words[i]);
+
+            if (wordWidth + currentLineWidth < maxLineWidth)
+            {
+                currentLine += words[i] + " ";
+                currentLineWidth += wordWidth;
+            }
+            else
+            {
+                Logger.Log(this, currentLineWidth.ToString() + ": " + currentLine);
+                output += "\n" + currentLine;
+                currentLine = "";
+                currentLineWidth = wordWidth;
+                currentLineCount++;
+            }
+        }
+
+        textUI.text = output;
+        text3d.text = output;
+    }
 }
